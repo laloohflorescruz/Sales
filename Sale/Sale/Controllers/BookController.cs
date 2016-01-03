f917f676aa530954c2f7b3327fcd9d4ae058c6cd
oriaraniar <oriaraniar@gmail.com>
2016-01-03 17:44:19 +0400

    Первые запросы дали ответ

---
+++ Sale/Sale/Controllers/BookController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Web.Http;
+using Sale.EF;
+using Sale.Models.Model;
+
+namespace Sale.Controllers
+{
+    public class BookController : ApiController
+    {
+
+        private readonly Context _context = new Context();
+
+        // GET: api/Book
+        public IEnumerable<Book> Get()
+        {
+            return _context.Books;
+        }
+
+        // GET: api/Book/5
+        public Book Get(int id)
+        {
+            var book = _context.Books.Find(id);
+            return book;
+        }
+
+        // POST: api/Book
+        public void Post([FromBody]Book book)
+        {
+            _context.Books.Add(book);
+            _context.SaveChanges();
+        }
+
+        // PUT: api/Book/5
+        public void Put(int id, [FromBody]Book book)
+        {
+            if (id != book.Id) return;
+            _context.Entry(book).State = EntityState.Modified;
+
+            _context.SaveChanges();
+        }
+
+        // DELETE: api/Book/5
+        public void Delete(int id)
+        {
+            var book = _context.Books.Find(id);
+            if (book == null) return;
+            _context.Books.Remove(book);
+            _context.SaveChanges();
+        }
+    }
+}
