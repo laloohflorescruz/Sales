6e68abfc83bfbb7c1590d883bc7304ce48e3f061
oriaraniar <oriaraniar@gmail.com>
2016-01-03 19:35:40 +0400

    Первая страница. Пока статика

---
+++ Sale/Sale/Controllers/Book2Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sale.Controllers
+{
+    public class Book2Controller : Controller
+    {
+        // GET: Book2
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
