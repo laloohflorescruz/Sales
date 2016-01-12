a206598af22966fdd3a6d2b88421e6178f271032
oriaraniar <oriaraniar@gmail.com>
2016-01-12 20:33:13 +0400

    Ангулар окончательно ожил

---
+++ Sale/Sale/Controllers/Book3Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Sale.Controllers
+{
+    public class Book3Controller : Controller
+    {
+        // GET: Book3
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
